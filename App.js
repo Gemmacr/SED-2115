e4591cc577e37d0ed74129e33c2943e59a4036f0
Gemmacr <146110914+Gemmacr@users.noreply.github.com>
2024-09-16 11:56:53 -0400

    App.js
    Lab 2

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Name from "./component/Name";
+
+
+function App() {
+console.log("come on guys keep up");
+return (
+    <div>
+      <h1>Gemma</h1>
+      <Name />
+    </div>
+  );
+}
+
+export default App;
