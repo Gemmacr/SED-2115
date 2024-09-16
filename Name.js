c05eaed59284f28422868e6b93c59d5aa1eeb7e9
Gemmacr <146110914+Gemmacr@users.noreply.github.com>
2024-09-16 11:56:11 -0400

    Name.js
    Lab 2

---
+++ Name.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+
+export default function Name() {
+    const first = "Wonderful"
+  return (
+    <div>
+      <h1>Hello Everyon</h1>
+      {first}
+      <p>Hi </p>
+    </div>
+    )
+}
