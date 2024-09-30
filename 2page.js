e13be73a8b78fefc40c1f99569baa048271bf945
Gemmacr <146110914+Gemmacr@users.noreply.github.com>
2024-09-30 15:10:25 -0400

    Create 2page.js
    Added a button to bring the user to another page but the code does not work.

---
+++ 2page.js
@@ -0,0 +1,25 @@
+<html lang="eng">
+<head>
+
+
+
+ <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>How to create a button with link</title>
+</head>
+<body>
+    <p>How to create button with link</p>
+
+<a href="http://localhost:3000/">Google</a>
+<button onclick="go to link(this)" value="window.location.href='http://localhost:3000/'">Google</button>
+
+
+
+
+
+
+   <script>
+        // Your JavaScript code can go here
+    </script>
+</body>
+</html>
